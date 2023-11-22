945ce16b2af879c4e683af0bfd630df245700739
Your Name <you@example.com>
2023-11-22 22:31:45 +0100

    update

--- my-app/App.tsx
+++ my-app/App.tsx
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Avatar from './components/avatar';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Avatar/>
     </View>
   );
 }
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#0D0F13',
     alignItems: 'center',
     justifyContent: 'center',
   },
